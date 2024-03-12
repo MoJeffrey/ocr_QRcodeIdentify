18834e6ecc1a4b8f5c7d4ca294dcb0608db2f9c8
Jeffrey <MoJeffreyNg@gmail.com>
2024-03-12 11:18:01 +0800

    (BE-??? ) 添加注解

--- src/Tools/ImageProcessing.py
+++ src/Tools/ImageProcessing.py
@@ -33,6 +33,14 @@ class ImageProcessing:
 
     @staticmethod
     def show(partitionList) -> None:
+        """
+        把拍摄的图片显示
+        linux 系统自行配置GUI 层
+
+        执行后按任意键则可跟换下一张图片。无需关闭窗口
+        :param partitionList:
+        :return:
+        """
         index = 0
         for partition in partitionList:
             cv2.imshow(str(index), partition)
@@ -42,6 +50,15 @@ class ImageProcessing:
         cv2.destroyAllWindows()
 
     def partition(self) -> list:
+        """
+        图片分割
+
+        分割的个数根据 配置文档，输入的高有多少个和宽有多少个图片为分割标准
+
+        e.g config.IMG_HIGH_QUANTITY = 2;config.IMG_WIDTH_QUANTITY=2
+        则： 平均分割成四张图片
+        :return:
+        """
         height, width = self.__img.shape[:2]
         self.__partitionList = []
 
